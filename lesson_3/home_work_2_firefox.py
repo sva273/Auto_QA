2b2587d68db4b29baacbf96d18c8557154824304
sva273 <spakv577@gmail.com>
2025-08-22 13:50:19 +0200

    update home_work_2_firefox.py

--- lesson_3/home_work_2_firefox.py
+++ lesson_3/home_work_2_firefox.py
@@ -2,6 +2,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
 
 
 # Настройка драйвера для Firefox с использованием
@@ -11,15 +12,17 @@ driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()
 # Изменение размера окна
 driver.fullscreen_window()
 
-
 # Открытие сайта
-driver.get("https://itcareerhub.de/ru#rec717852307")
+driver.get("https://itcareerhub.de/ru")
 
+about_link = driver.find_element(By.LINK_TEXT, "О нас")
+about_link.click()
 # Задержка перед закрытием браузера
-
+sleep(3
+      )
 
 # Сохранение скриншота
-driver.save_screenshot("./itc_payment_screenshot_2.png") # Сохранение скриншота в текущую папку
+driver.save_screenshot("./itc_payment_screenshot_4.png") # Сохранение скриншота в текущую папку
 
 sleep(3)
 driver.quit()
